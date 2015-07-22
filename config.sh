81f060a5043c2e72af649bcc40472880c4aa223b
Mark Haslehurst <mhaslehurst@users.noreply.github.com>
2015-07-22 11:29:01 +0100

    Slight reduction of config.sh, also fixed indentation

--- config.sh
+++ config.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 THIS_SCRIPT=$BASH_SOURCE
+THIS_SCRIPT_DIR="$(readlink -e `dirname $THIS_SCRIPT`)"
 
 if [ "$BASH_SOURCE" = "$0" ]
-        then
-                echo "This script must be sourced!"
-                exit 1
+then
+    echo "This script must be sourced!"
+    exit 1
 fi
 
 export MAXCOMPILERDIR=/network-raid/opt/maxcompiler-2014.1.1
@@ -13,11 +14,4 @@ export MAXELEROSDIR=$MAXCOMPILERDIR/lib/maxeleros-sim
 export LD_PRELOAD=$MAXELEROSDIR/lib/libmaxeleros.so:$LD_PRELOAD
 export SLIC_CONF="$SLIC_CONF;use_simulation=${USER}Sim"
 export PATH=$MAXCOMPILERDIR/bin:$MAXELEROSDIR/utils:$PATH
-
-EXAMPLESDIR=`dirname $THIS_SCRIPT`
-
-export EXAMPLESDIR=`readlink -e $EXAMPLESDIR`
-
-echo Setting EXAMPLESDIR to $EXAMPLESDIR
-
-export PYTHONPATH="$EXAMPLESDIR/utils:$PYTHONPATH"
+export PYTHONPATH="$THIS_SCRIPT_DIR/utils:$PYTHONPATH"
