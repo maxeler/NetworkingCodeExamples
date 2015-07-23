607d423f9cc25e9f7309416d36e536951e175209
Mark Haslehurst <mhaslehurst@users.noreply.github.com>
2015-07-23 16:26:10 +0100

    Switched to fabricate and moved injector to separate sender directory

---
+++ Gateways/BinFIFO/runtime/run.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import build
+
+build.run_sim()
--- Gateways/BinFIFO/runtime/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-export MAXELEROSDIR=$MAXCOMPILERDIR/lib/maxeleros-sim
-export LD_PRELOAD=$MAXELEROSDIR/lib/libmaxeleros.so:$LD_PRELOAD
-export SLIC_CONF="$SLIC_CONF;use_simulation=${USER}Sim"
-
-make run_sim
-
