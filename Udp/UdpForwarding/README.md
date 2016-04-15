f37058189d71585184c3ee84bf57cd8b1ff971dc
Itay Greenspon <itay.gr@gmail.com>
2016-04-15 17:44:52 +0100

    More test skeleton

--- Udp/UdpForwarding/README.md
+++ Udp/UdpForwarding/README.md
@@ -1,28 +1,44 @@
-# My Project Title
+# Udp Forwarding
 
-This is a generic project template. My Project does: X, Y, Z.
+This example takes a a UDP multicast feed as its input.  
+When an incoming packet arrives, it performs some minimal processing to figure out if and where to forward it.  
+The destination is called a consumer and data is sent to consumers over UDP.  
+  
+  
 
 
 ## How to build
 
 ### Environment
+
+```bash
 source utils/config.sh
+```
 
 
 ### Bitstream
+
+```bash
 cd bitstream
 ant
+```
 
 
 ### Runtime
+
+```bash
 cd runtime
 cp <maxfile> .
 ./build.py
+````
 
 
 ## How to run
 
+```bash
 cd runtime
-./myApp
+./build.py run_sim
+```
+
 
 
--- Udp/UdpForwarding/runtime/udpforwarding.c
+++ Udp/UdpForwarding/runtime/udpforwarding.c
@@ -1,22 +1,69 @@
 #define _GNU_SOURCE
 
-#include <errno.h>
-#include <err.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
+#include "common.h"
 
 #include "MaxSLiCInterface.h"
 
 extern max_file_t *UdpForwarding_init();
+static struct in_addr netmask;
+static struct in_addr dfe_top_ip;
+static struct in_addr dfe_bot_ip;
+static struct in_addr multicast_ip;
+static struct in_addr *remote_ips;
 
-int main(int argc, char *argv[]) {
+static max_file_t *maxfile;
+static max_engine_t *engine;
 
-	max_file_t *maxfile = UdpForwarding_init();
-	max_engine_t * engine = max_load(maxfile, "*");
+#define PACKED __attribute__((packed))
+
+struct PACKED decision_rom {
+	uint8_t should_forward;
+	uint8_t consumer_id;
+};
+
+struct PACKED event {
+	uint64_t event_count;
+	uint8_t message_type;
+	uint8_t forward_decision;
+	uint8_t consumer_id;
+	uint64_t : 40;
+};
+
+static void configureDecision(size_t numConsmers);
+static void configureConsumers(size_t numConsumers);
+static void configureMulticastFeed();
+static void events_loop();
+
+uint16_t get_consumer_port(size_t consumer_index) {
+	return CONSUMER_DST_PORT + consumer_index;
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 5) {
+		printf("Usage: %s <Top IP> <Bot IP> <multicast_ip> <consumer_ip1> [consumer_ip2 consumer_ip3 ...]\n", argv[0]);
+		return 1;
+	}
+
+	size_t num_consumers = argc - 4;
+
+	inet_aton("255.255.255.0", &netmask);
+	printf("Local DFE Top @ %s\n", argv[1]);
+	inet_aton(argv[1], &dfe_top_ip);
+	printf("Local DFE Bot @ %s\n", argv[2]);
+	inet_aton(argv[2], &dfe_bot_ip);
+	printf("Multicast Feed @ %s:%d\n", argv[3], MULTICAST_PORT);
+	inet_aton(argv[3], &multicast_ip);
+
+	for (size_t i=0; i < num_consumers; i++) {
+		printf("Consumer %zd @ %s:%d\n", i, argv[4 + i], get_consumer_port(i));
+		inet_aton(argv[4 + i], &remote_ips[i]);
+	}
+
+
+	maxfile = UdpForwarding_init();
+	engine = max_load(maxfile, "*");
 
 
 	max_config_set_bool(MAX_CONFIG_PRINTF_TO_STDOUT, true);
@@ -25,12 +72,11 @@ int main(int argc, char *argv[]) {
 	max_run(engine, action);
 
 
-	/*
-	 * More code here
-	 */
+	configureDecision(num_consumers);
+	configureMulticastFeed();
+	configureConsumers(num_consumers);
 
-	
-
+	events_loop();
 
 
 	max_unload(engine);
@@ -38,3 +84,61 @@ int main(int argc, char *argv[]) {
 
 	return 0;
 }
+
+static void events_loop()
+{
+	void *event_buffer;
+	size_t num_slots = 512;
+	size_t slot_size = sizeof(struct event);
+
+	if (posix_memalign(&event_buffer, 4096, num_slots * slot_size)) {
+		err(1, "Couldn't allocate events buffer");
+	}
+
+	max_llstream_t *event_stream = max_llstream_setup(engine, "events", num_slots, slot_size, event_buffer);
+
+	while (true) {
+		void *event_slot;
+		if (max_llstream_read(event_stream, 1, &event_slot) == 1) {
+			struct event *the_event = event_slot;
+			printf("Event [%lu] type 0x%02x --> ", the_event->event_count, the_event->message_type);
+			if (the_event->forward_decision == 0) printf("DROPPED\n");
+			else printf("Forwarded to consumer %d\n", the_event->consumer_id);
+			max_llstream_read_discard(event_stream, 1);
+		}
+	}
+}
+
+static void configureDecision(size_t numConsmers)
+{
+	max_actions_t *action = max_actions_init(maxfile, NULL);
+	struct decision_rom d;
+	uint64_t *v = (void *)&d;
+
+	for (size_t msgType = 0; msgType < 256; msgType++) {
+		d.consumer_id = msgType % numConsmers;
+		d.should_forward = msgType % 2 == 0;
+		max_set_mem_uint64t(action, "fwd", "decisionRom", msgType, *v);
+	}
+	max_run(engine, action);
+}
+
+static void configureConsumers(size_t numConsumers)
+{
+	printf("Setting up consumer %zd sockets...", numConsumers);
+	max_ip_config(engine, MAX_NET_CONNECTION_QSFP_BOT_10G_PORT1, &dfe_bot_ip, &netmask);
+	for (size_t i=0; i< numConsumers; i++) {
+		max_udp_socket_t *consumer_socket = max_udp_create_socket_with_number(engine, "Consumers", i);
+		max_udp_bind(consumer_socket, CONSUMER_SRC_PORT);
+		max_udp_connect(consumer_socket, &remote_ips[i], get_consumer_port(i));
+	}
+}
+
+static void configureMulticastFeed()
+{
+	printf("Setting up Multicast feed socket...");
+	max_ip_config(engine, MAX_NET_CONNECTION_QSFP_TOP_10G_PORT1, &dfe_top_ip, &netmask);
+	max_udp_socket_t *dfe_socket = max_udp_create_socket(engine, "UdpMulticastFeed");
+	max_udp_bind_ip(dfe_socket, &multicast_ip, MULTICAST_PORT);
+}
+
